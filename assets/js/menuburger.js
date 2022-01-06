8734487ea759d3a9fe94cf314e248ac80f524392
Alois Patoor <palois89@posteo.net>
2022-01-06 08:44:52 +0100

    Travail sur la version mobile du header

--- assets/js/menuburger.js
+++ assets/js/menuburger.js
@@ -5,3 +5,17 @@ menuBurger.addEventListener("click", function(){
     let dropdown = document.getElementById("dropdown");
     dropdown.classList.toggle("displayNone");
 });
+
+
+// DROPDOWN DES MEMBRES
+let member = document.getElementById("desktopMember");
+member.addEventListener("mouseenter", function(){
+    let dropdownMember = document.querySelector(".memberDropdown");
+    dropdownMember.classList.toggle("displayNone");
+});
+// VERSION MOBILE
+let mobileMember = document.getElementById("mobileMember");
+mobileMember.addEventListener("click", function(){
+    let dropdownMobileMember = document.querySelector(".memberMobileDropdown");
+    dropdownMobileMember.classList.toggle("displayNone");
+});
