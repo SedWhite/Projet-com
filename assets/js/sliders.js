155b8a7af72df12119696e6dcba1019b47a3ccd2
Alexis Doyen <alexisdoyen24@gmail.com>
2022-01-06 11:11:45 +0100

    ajout header slider

--- assets/js/sliders.js
+++ assets/js/sliders.js
@@ -1,13 +1,24 @@
 // CAROUSSEL 
-let slide = new Array("foret-peuplier.jpg", "paysage-montagne.jpg", "chemin-automne.jpg", "prairie-alpes.jpg"); // PREVOIR DE FAIRE UN ARRAY DIFFERENT POUR CHAQUE CARROUSEL
-let number = 0;
-
-function ChangeSlide(sens) {
-    number = number + sens;
-    if (number < 0)
-        number = slide.length - 1;
-    if (number > slide.length - 1)
-        number = 0;
-    document.getElementById("slide").src = slide[number];
-    setInterval("ChangeSlide(1)", 5000); // POUR DEFILEMENT AUTOMATIQUE TOUTES LES 5 SECONDES
+window.onload = function(){
+    var slides = document.getElementsByClassName('carousel-item'),
+        addActive = function(slide) {slide.classList.add('active')},
+        removeActive = function(slide) {slide.classList.remove('active')};
+    addActive(slides[0]);
+    
+    setInterval(function (){
+      for (var i = 0; i < slides.length; i++){
+        if (i + 1 == slides.length) {
+          addActive(slides[0]);
+        //   slides[0].style.zIndex = 100;
+          setTimeout(removeActive, 350, slides[i]); //Doesn't be worked in IE-9
+          break;
+        }
+        if (slides[i].classList.contains('active')) { 
+          slides[i].removeAttribute('style');
+          setTimeout(removeActive, 350, slides[i]); //Doesn't be worked in IE-9
+          addActive(slides[i + 1]);
+          break;
+        }
+      } 
+    }, 4000);
 }
